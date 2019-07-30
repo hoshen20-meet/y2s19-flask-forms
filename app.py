7281da3599a11b86cddb10958a3c55723e4f4b93
myname21-meet <myname21@meet.mit.edu>
2019-07-30 07:05:32 -0400

    flask forms lab part 2

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from databases import *
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html', students=query_all())
 
-@app.route('/student/<int:student_id>')
+@app.route('/student/<int:student_id>', methods=(['GET','POST']))
 def display_student(student_id):
+ 
     return render_template('student.html', student=query_by_id(student_id))
+
+@app.route('/add', methods=(['GET', 'POST']))
+def add_student_route():
+    if request.method == 'GET':
+        return render_template('add.html')
+    else:
+        print('Recieved POST request')
+        name = request.form['student_name']
+        year = request.form['student_year']
+        add_student(name, year)
+        return render_template('add.html')
+
+@app.route('/delete/<int:student_id>', methods=['POST'])
+def delete_student(student_id):
+           delete_student_id(student_id)
+           return redirect(url_for('home'))
+    
 
 #Create an '/add' route here:
 
